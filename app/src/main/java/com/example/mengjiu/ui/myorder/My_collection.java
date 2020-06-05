1d3d2fb544b77e9c405d9ed718080101773a15da
ll <63940924+liuluupup@users.noreply.github.com>
2020-06-05 09:50:03 +0800

    0058新增收藏页面,优惠券页面,钱包页面

---
+++ app/src/main/java/com/example/mengjiu/ui/myorder/My_collection.java
@@ -0,0 +1,16 @@
+package com.example.mengjiu.ui.myorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mengjiu.R;
+
+public class My_collection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_collection);
+    }
+}
