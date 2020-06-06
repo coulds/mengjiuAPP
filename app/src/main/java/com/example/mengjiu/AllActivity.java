9b58a0fb821ae1ab65234ecacb012dea3b86e0f1
卢鹏 <1598156227@qq.com>
2020-06-06 09:45:46 +0800

    0073 完成账单界面的待评论界面与完成订单界面

--- app/src/main/java/com/example/mengjiu/AllActivity.java
+++ app/src/main/java/com/example/mengjiu/AllActivity.java
@@ -11,6 +11,8 @@ public class AllActivity extends AppCompatActivity {
     private final static String TAG="Mengjiu";
     private Button allb2;
     private Button allb3;
+    private Button allb4;
+    private Button allb5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,22 @@ public class AllActivity extends AppCompatActivity {
         allb3.setOnClickListener(v->{
             Log.d(TAG,"点击待配送订单！前往待配送订单页面");
             Intent intent=new Intent(AllActivity.this,ShippingActivity.class);
+            startActivity(intent);
+        });
+
+        //点击进入待评价订单页
+        allb4=findViewById(R.id.all_b4);
+        allb4.setOnClickListener(v->{
+            Log.d(TAG,"点击待评论订单！前往待评论订单页面");
+            Intent intent=new Intent(AllActivity.this,EvaluatedActivity.class);
+            startActivity(intent);
+        });
+
+        //点击进入完成订单页
+        allb5=findViewById(R.id.all_b5);
+        allb5.setOnClickListener(v->{
+            Log.d(TAG,"点击完成订单！前往完成订单页面");
+            Intent intent=new Intent(AllActivity.this,FinishActivity.class);
             startActivity(intent);
         });
     }
